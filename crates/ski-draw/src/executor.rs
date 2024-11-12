42b8758836affee8099409c9e3a36a884e11ceb5
Aadi <goooolok727@gmail.com>
2024-11-12 17:04:55 +0530

    start executor

---
+++ crates/ski-draw/src/executor.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct ForegroundExecutor {}
+
+impl ForegroundExecutor {}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct BackgroundExecutor {}
+
+impl BackgroundExecutor {}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Dispatcher {}
+
+impl Dispatcher {}
--- crates/ski-draw/src/lib.rs
+++ crates/ski-draw/src/lib.rs
@@ -2,4 +2,5 @@ pub mod app;
 pub mod gpu;
 pub mod renderer;
 pub use renderer::Renderer;
+pub(crate) mod executor;
 pub mod window;
