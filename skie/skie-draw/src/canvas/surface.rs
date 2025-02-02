5d23ddd63c3aee7375776ff9d1eaf35762bcf8c2
Aadi <goooolok727@gmail.com>
2025-02-02 13:30:45 +0530

    add msaa

--- skie/skie-draw/src/canvas/surface.rs
+++ skie/skie-draw/src/canvas/surface.rs
@@ -18,6 +18,7 @@ pub struct CanvasSurfaceConfig {
     pub height: u32,
     pub format: gpu::TextureFormat,
     pub usage: gpu::TextureUsages,
+    pub(crate) msaa_sample_count: u32,
 }
 
 impl Default for CanvasSurfaceConfig {
@@ -27,6 +28,33 @@ impl Default for CanvasSurfaceConfig {
             height: 0,
             format: gpu::TextureFormat::Rgba8Unorm,
             usage: gpu::TextureUsages::RENDER_ATTACHMENT,
+            msaa_sample_count: 1,
         }
     }
+}
+
+pub fn create_mssa_view(
+    gpu: &GpuContext,
+    config: &CanvasSurfaceConfig,
+) -> Option<wgpu::TextureView> {
+    (config.msaa_sample_count > 1).then(|| {
+        let texture_format = config.format;
+
+        gpu.device
+            .create_texture(&wgpu::TextureDescriptor {
+                label: Some("skie_msaa_texture"),
+                size: wgpu::Extent3d {
+                    width: config.width,
+                    height: config.height,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: config.msaa_sample_count.max(1),
+                dimension: wgpu::TextureDimension::D2,
+                format: texture_format,
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                view_formats: &[texture_format],
+            })
+            .create_view(&wgpu::TextureViewDescriptor::default())
+    })
 }
